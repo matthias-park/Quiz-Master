7c1f8658eb3dfdeccf5bc27b83f56eddc862fdca
Joons <matthiaspark0514@gmail.com>
2024-03-09 13:40:16 -0800

    Feat: For loop for button displays

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
+using System.Linq;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
     // Start is called before the first frame update
     void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for(int i = 0; i < answerButtons.Length; i++) 
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
     }
 
 }
